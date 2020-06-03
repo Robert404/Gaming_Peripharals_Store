70f721406d56b38a95d8ed69314b37b6b8158490
Robert <robners404@gmail.com>
2020-06-03 15:14:42 +0400

    User Sign In, Sign Up

---
+++ Peripharals_Store/Peripharals_Store/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Peripharals_Store.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peripharals_Store.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
